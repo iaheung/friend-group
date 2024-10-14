cf4f2732904b7cb83de127989ba37cefe5708741
iaheung <ianhyheung@gmail.com>
2024-10-14 10:44:13 +0100

    Added group array

--- group.py
+++ group.py
@@ -2,4 +2,9 @@
 
 # Your code to go here...
 
-my_group =
+
+my_group = [
+    {'name': 'Jill', 'age':26, 'job':'Biologist','connection': [('friend','Zalika'),('partner','John')]},
+    {'name': 'Zalika', 'age':28, 'job':'artist','connection': [('friend','Jill')]},
+    {'name': 'John', 'age':27, 'job':'writer','connection': [('partner','Jill')]},
+    {'name': 'Nash', 'age':34, 'job':'chef','connection': [('landlord','Zalika'),('cousin','John')]}]
