8c8312f0ec8d352938e1f93f3cc4ea6132ef0011
iaheung <ianhyheung@gmail.com>
2024-10-14 10:42:58 +0100

    Revert "Dictionary of a group of acquaintances"
    This reverts commit f6292e423202ee664a82bc198580f7101f743b6e.

--- group.py
+++ group.py
@@ -2,12 +2,4 @@
 
 # Your code to go here...
 
-
-my_group = [
-    {'name': 'Jill', 'age':26, 'job':'Biologist','connection': [('friend','Zalika'),('partner','John')]},
-    {'name': 'Zalika', 'age':28, 'job':'artist','connection': [('friend','Jill')]},
-    {'name': 'John', 'age':27, 'job':'writer','connection': [('partner','Jill')]},
-    {'name': 'Nash', 'age':34, 'job':'chef','connection': [('landlord','Zalika'),('cousin','John')]}]
-
-
-Jill is 26, a biologist and she is Zalika's friend and John's partner.
+my_group =
